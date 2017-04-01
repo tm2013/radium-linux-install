1b2ee2ffced8a2db1742d7ee7411cea44935e1b5
Tim Mesker <timcrypto@gmail.com>
2017-04-01 14:36:02 -0400

    Update radium-install_sh.sh

--- radium-install_sh.sh
+++ radium-install_sh.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apt-get -y update && apt-get -y install git build-essential libssl-dev libdb++-dev libboost-all-dev libqrencode-dev libminiupnpc-dev qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools && git clone https://github.com/ProjectRadium/Radium.git && cd Radium && qmake USE_03=1 RELEASE=1 && make && cd src && make -f makefile.unix && strip Radiumd
+apt-get -y update && apt-get -y install git build-essential libssl-dev libdb++-dev libboost-all-dev libqrencode-dev libminiupnpc-dev qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools && git clone https://github.com/RadiumCore/Radium.git && cd Radium && qmake USE_03=1 RELEASE=1 && make && cd src && make -f makefile.unix && strip Radiumd
